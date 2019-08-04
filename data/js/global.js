f87107b6408d5c35f1dc4d791f838041923fc229
CJ <cjpaisdev@gmail.com>
2019-08-04 03:41:31 +0200

    fix js

--- data/js/global.js
+++ data/js/global.js
@@ -1,9 +1,12 @@
-obj_list = document.getElementsByTagName('object');
-for (obj = 0; obj < obj_list.length; obj++) {
-  svg = obj_list[obj].getSVGDocument().getElementsByTagName('svg')[0]
-  bbox = svg.getBBox()
-  viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ")
-  svg.setAttribute("viewBox", viewBox)
-  svg.setAttribute("height", bbox.height)
-  svg.setAttribute("width", bbox.width)
-}
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("document loaded")
+  obj_list = document.getElementsByTagName('object');
+  for (obj = 0; obj < obj_list.length; obj++) {
+    svg = obj_list[obj].getSVGDocument().getElementsByTagName('svg')[0]
+    bbox = svg.getBBox()
+    viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ")
+    svg.setAttribute("viewBox", viewBox)
+    svg.setAttribute("height", bbox.height)
+    svg.setAttribute("width", bbox.width)
+  }
+});
