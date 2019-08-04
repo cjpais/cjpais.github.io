4f10bbcdce64ede2c73d32a773dbc7a0db29bb30
CJ <cjpaisdev@gmail.com>
2019-08-04 03:38:34 +0200

    add some js to fix the svg

---
+++ data/js/global.js
@@ -0,0 +1,9 @@
+obj_list = document.getElementsByTagName('object');
+for (obj = 0; obj < obj_list.length; obj++) {
+  svg = obj_list[obj].getSVGDocument().getElementsByTagName('svg')[0]
+  bbox = svg.getBBox()
+  viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ")
+  svg.setAttribute("viewBox", viewBox)
+  svg.setAttribute("height", bbox.height)
+  svg.setAttribute("width", bbox.width)
+}
