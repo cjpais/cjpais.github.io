d7813bb3c9d5db0fbea7b03ed35aed2e4b2bf6c0
CJ <cjpaisdev@gmail.com>
2019-08-05 03:02:57 +0200

    updated pages from script: TempIdx.pdf

--- auto_update.sh
+++ auto_update.sh
@@ -2,7 +2,7 @@
 
 while true; do
   inotifywait -r ~/Dropbox/Notability/cjpais.com/
-  python process.py ~/Dropbox/Notability/cjpais.com/ res/svg commit
-  # hacky debounce
+  # hacky debounce + make sure new file is picked up
   sleep 5
+  python process.py ~/Dropbox/Notability/cjpais.com/ res/svg commit
 done
